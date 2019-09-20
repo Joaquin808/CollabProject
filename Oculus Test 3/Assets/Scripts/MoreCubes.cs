93a5b1b97170450b4eb739cdadbc43231cd43afa
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-09-19 22:15:40 -0400

    Testing UI interaction
    Creating a variety of different UI options and testing them out to make sure I can make UI work properly.

---
+++ Oculus Test 3/Assets/Scripts/MoreCubes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreCubes : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    // Update is called once per frame
+    public void Cube()
+    {
+        Instantiate(cubePrefab);
+    }
+}
---
+++ Oculus Test 3/Assets/Scripts/ShowCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCanvas : MonoBehaviour
+{
+    public GameObject Canvas;
+    public bool CanvasIsShown = false;
+
+    public void DisplayCanvas()
+    {
+        if (!CanvasIsShown)
+        {
+            Canvas.SetActive(true);
+            CanvasIsShown = true;
+        }
+        else
+        {
+            Canvas.SetActive(false);
+            CanvasIsShown = false;
+        }
+        
+    }
+}
