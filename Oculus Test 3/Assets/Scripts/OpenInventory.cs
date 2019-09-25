031e2754a3db984c487c176c6b954cd1eff9f1cf
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-09-24 21:06:42 -0400

    Added the ability to open inventory menu when the player looks at their watch

---
+++ Oculus Test 3/Assets/Scripts/OpenInventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenInventory : MonoBehaviour
+{
+    public GameObject Inventory;
+    public GameObject InventoryCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventory.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == InventoryCollider)
+        {
+            Inventory.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == InventoryCollider)
+        {
+            Inventory.SetActive(false);
+        }
+    }
+}
