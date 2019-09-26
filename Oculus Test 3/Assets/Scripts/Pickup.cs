51556824d6c9f6d5ff63af53d4730157a0528d61
Platinumsuta <54861300+Platinumsuta@users.noreply.github.com>
2019-09-26 15:21:47 -0400

    UI Spawning

--- Oculus Test 3/Assets/Scripts/Pickup.cs
+++ Oculus Test 3/Assets/Scripts/Pickup.cs
@@ -8,11 +8,13 @@ public class Pickup : InventoryItem
 {
     public GameObject ObjectToSpawn;
     public GameObject Hand;
+    
 
     public override void Use()
     {
         base.Use();
-        Instantiate(ObjectToSpawn, Hand.transform);
+        GameObject watch = GameObject.FindGameObjectWithTag("Watch");
+        GameObject spawned = Instantiate(ObjectToSpawn, watch.transform.position, Quaternion.identity);
         Inventory.Instance.Remove(this);
         Debug.Log("Use");
     }
