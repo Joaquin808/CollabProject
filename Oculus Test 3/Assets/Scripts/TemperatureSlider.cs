cf4591de475e4929e5ea95b23ae60066c992b1b4
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-09-27 15:46:56 -0400

    Started Work on Slider Puzzle
    Worked on basic mechanics for slider puzzle with basic cube. Also created blank scenes for the dome and house levels of the game. Also changed the name of SampleScene to BunkerScene

---
+++ Oculus Test 3/Assets/Scripts/TemperatureSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Temperature Puzzle, Slider Mechanics
+
+public class TemperatureSlider : MonoBehaviour
+{
+    public bool isStopped;
+    Vector3 pos1 = new Vector3(5,0,0);
+    Vector3 pos2 = new Vector3(5, -1, 0);
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BarMovement();
+    }
+
+    //Sets up movement for sliding bar
+    void BarMovement() 
+    {
+        if (isStopped == false)
+        {
+            transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+        }
+    }
+}
