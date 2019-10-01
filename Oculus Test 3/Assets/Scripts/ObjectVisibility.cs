3d338a98929614630a7df73986abd8656efe4327
Platinumsuta <54861300+Platinumsuta@users.noreply.github.com>
2019-10-01 16:25:58 -0400

    Oops
    Nailed it /s

--- Oculus Test 3/Assets/Scripts/ObjectVisibility.cs
+++ Oculus Test 3/Assets/Scripts/ObjectVisibility.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ObjectVisibility : MonoBehaviour
 {
+    List<string> collisionList = new List<string>();
 
     // Use this for initialization
     void Start()
     {
-        GetComponent<Renderer>().enabled = true;
+        
+        GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -18,6 +21,36 @@ public class ObjectVisibility : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        GetComponent<Renderer>().enabled = false;
+        if (!collisionList.Contains(collision.gameObject.tag))
+        {
+            collisionList.Add(collision.gameObject.tag);
+        }
+      
+        if (collisionList.Contains("GhostPipe") && collisionList.Contains("Tool"))
+            { 
+            GetComponent<Renderer>().enabled = true;
+            collisionList.Remove("GhostPipe");
+            collisionList.Remove("Tool");
+            if (collision.gameObject.tag == "GhostPipe")
+                {
+                Destroy(collision.gameObject);
+                }
+            }
+        }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "GhostPipe")
+        {
+            collisionList.Remove("GhostPipe");
+
+        }
+
+       if (collision.gameObject.tag == "Tool")
+        {
+            collisionList.Remove("Tool");
+        }
     }
+     
 }
+
