f03bc2b6d00cce8eb980ea498ba07ce18d0a0513
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-10-08 11:28:48 -0400

    Clock

---
+++ Oculus Test 3/Assets/Scripts/InventoryMenu/ClockTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockTime : MonoBehaviour
+{
+    public Text DisplayText;
+    int Hour = 0;
+    int Minutes = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Minutes++;
+        DisplayText.text = Hour.ToString("0:") + Minutes.ToString("00");
+        if (Minutes >= 60)
+        {
+            Hour++;
+            Minutes = 0;
+        }
+    }
+}
