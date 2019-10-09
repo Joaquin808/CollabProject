36bb07da3fe948a8b8dea7c8472967b4bf428ee4
Sydney Brown <sbbrown@email.peace.edu>
2019-10-09 17:20:49 -0400

    HouseGreyBox & Navmeshing & Elevator

---
+++ Oculus Test 3/Assets/Scripts/Elevator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+	
+	Vector3 moveDirection = Vector3.up;	//elevator starts down
+	float liftSpeed = 5;
+	Vector3 startPos;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= 30) {
+			moveDirection = Vector3.down;
+		} else if (transform.position.y <= startPos.y) {
+			moveDirection = Vector3.up;
+		}
+		
+		transform.Translate (moveDirection * Time.deltaTime * liftSpeed);
+			
+    }
+}
