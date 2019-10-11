bde4e6b6b0f09a91ec7aae11ffe6e754ae63b498
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-10-11 16:20:08 -0400

    Journal entry popups
    Worked on Journal Entry Popup view of the text

--- Oculus Test 3/Assets/Scripts/InventoryMenu/Pickup.cs
+++ Oculus Test 3/Assets/Scripts/InventoryMenu/Pickup.cs
@@ -11,6 +11,13 @@ public class Pickup : InventoryItem
     public override void Use()
     {
         base.Use();
+        if (ItemType == "Journal")
+        {
+            GameObject JournalCanvas = GameObject.FindGameObjectWithTag("JournalCanvas");
+            GameObject spawn = Instantiate(ObjectToSpawn, JournalCanvas.transform);
+            // play journal entry sound
+            return;
+        }
         GameObject watch = GameObject.FindGameObjectWithTag("Watch");
         GameObject spawned = Instantiate(ObjectToSpawn, watch.transform.position, Quaternion.identity);
         Inventory.Instance.Remove(this);
