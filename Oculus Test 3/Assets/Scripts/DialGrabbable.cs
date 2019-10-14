61d82df7470cfe023a6eac80bdb3eb2ae640cb27
Platinumsuta <54861300+Platinumsuta@users.noreply.github.com>
2019-10-13 22:19:50 -0400

    Dial Mechanic working
    Yosh

---
+++ Oculus Test 3/Assets/Scripts/DialGrabbable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialGrabbable : OVRGrabbable
+{
+    //This script is taken from Valem's video
+    public Transform dial;
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(Vector3.zero, Vector3.zero);
+
+        transform.position = dial.transform.position;
+        transform.rotation = dial.transform.rotation;
+    }
+}
