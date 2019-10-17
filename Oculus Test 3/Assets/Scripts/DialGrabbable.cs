258795ebf3cf31c233a172401cb12acb0d5a4d91
Platinumsuta <54861300+Platinumsuta@users.noreply.github.com>
2019-10-16 22:19:52 -0400

    Radio, knobs turn but they drift
    Got basic idea for radio, need to work on it more however. Need some critique on the way i'm doing it
    
    plus need to not make the knobs drift

--- Oculus Test 3/Assets/Scripts/DialGrabbable.cs
+++ Oculus Test 3/Assets/Scripts/DialGrabbable.cs
@@ -6,11 +6,15 @@ public class DialGrabbable : OVRGrabbable
 {
     //This script is taken from Valem's video
     public Transform dial;
+    float x;
 
     public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
     {
         base.GrabEnd(Vector3.zero, Vector3.zero);
-
+        /*
+        x = transform.eulerAngles.x;
+        transform.rotation = Quaternion.Euler(x, 0, 0);
+        */
         transform.position = dial.transform.position;
         transform.rotation = dial.transform.rotation;
     }
