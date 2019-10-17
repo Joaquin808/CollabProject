60c590e98ae126267c90bc11280d68493f8411f4
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-10-17 18:40:53 -0400

    Journal Entries and inventory tabs
    All journal entry text has been added to all objects. Inventory tabs now turn gray when they are not active so the player knows which tab they're in.

---
+++ Oculus Test 3/Assets/ToggleActive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleActive : MonoBehaviour
+{
+    Toggle Self;
+    public GameObject Content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Self = GetComponent<Toggle>();
+        Self.onValueChanged.AddListener(OnToggleValueChanged);
+
+        ColorBlock cb = Self.colors;
+        cb.normalColor = Content.activeInHierarchy ? Color.white : Color.gray;
+        cb.highlightedColor = Content.activeInHierarchy ? Color.white : Color.gray;
+        Self.colors = cb;
+    }
+
+    void OnToggleValueChanged(bool IsOn)
+    {
+        ColorBlock cb = Self.colors;
+        cb.normalColor = IsOn ? Color.white : Color.gray;
+        cb.highlightedColor = IsOn ? Color.white : Color.gray;
+        Self.colors = cb;
+    }
+}
