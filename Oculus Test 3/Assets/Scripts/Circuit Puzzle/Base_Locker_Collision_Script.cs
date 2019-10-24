50f2d7436c122628d98d9761374db891c23d1044
LegendaryBumz <54956491+LegendaryBumz@users.noreply.github.com>
2019-10-24 13:50:01 -0400

    Added Power Puzzle Scripts

---
+++ Oculus Test 3/Assets/Scripts/Circuit Puzzle/Base_Locker_Collision_Script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_Locker_Collision_Script : MonoBehaviour
+{
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Circuit")
+        {
+            Debug.Log("That seems to fit...");
+        }
+    }
+}
---
+++ Oculus Test 3/Assets/Scripts/Circuit Puzzle/PositionReferences.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionReferences : MonoBehaviour
+{
+    public Transform[] positions;
+    private int index = 0;
+
+    public Vector3 GetNextPosition()
+    {
+        Vector3 result = positions[index].localPosition;
+        index = index + 1;
+        return result;
+    }
+}
