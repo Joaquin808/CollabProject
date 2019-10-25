e16f6b7f83de2adffadbd935b5f0d49580c27f4f
LegendaryBumz <54956491+LegendaryBumz@users.noreply.github.com>
2019-10-25 14:17:21 -0400

    Circuit Puzzle Update

---
+++ Oculus Test 3/Assets/Scripts/Circuit Puzzle/Circuit_panel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circuit_panel : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject panelLocker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelLocker.transform.parent = panel.transform;
+        Debug.Log("Panel Locker's Parent: " + panelLocker.transform.parent.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
