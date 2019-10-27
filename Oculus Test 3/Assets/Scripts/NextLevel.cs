10d91bb55ff802cbf8c0d0f6bbca0a18669eb77d
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-10-26 21:55:43 -0400

    Title Scene Changes

--- Oculus Test 3/Assets/Scripts/NextLevel.cs
+++ Oculus Test 3/Assets/Scripts/NextLevel.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scenes : MonoBehaviour
+public class NextLevel : MonoBehaviour
 {
     public GameObject cube;
     public GameObject spawn;
-    /*private static void LoadScene()
-    {
 
-    }*/
+    // Start is called before the first frame update
     void Start()
     {
         Transform transform = spawn.GetComponent<Transform>();
     }
-    void CreateCubes()
+
+    public void SpawnCubes()
     {
         Instantiate(cube, transform);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
