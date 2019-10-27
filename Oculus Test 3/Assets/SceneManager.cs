1ec695bc9e7456c8d9b9c01005e66852193567a5
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-10-26 21:01:28 -0400

    Title Scene Updates

---
+++ Oculus Test 3/Assets/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject cube;
+    public Transform spawn;
+    /*private static void LoadScene()
+    {
+
+    }*/
+
+    void CreateCubes()
+    {
+        Instantiate(cube, spawn);
+    }
+}
---
+++ Oculus Test 3/Assets/Scripts/Scenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenes : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject spawn;
+    /*private static void LoadScene()
+    {
+
+    }*/
+    void Start()
+    {
+        Transform transform = spawn.GetComponent<Transform>();
+    }
+    void CreateCubes()
+    {
+        Instantiate(cube, transform);
+    }
+}
