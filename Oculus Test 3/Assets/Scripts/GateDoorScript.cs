6dfef1c3d7d4a5001ac7bd50975f8bdb4d1f274c
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-10-26 21:36:43 -0400

    Gate door

--- Oculus Test 3/Assets/Scripts/GateDoorScript.cs
+++ Oculus Test 3/Assets/Scripts/GateDoorScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class GateDoorScript : MonoBehaviour
 {
-    float liftSpeed = 5;
+    float liftSpeed = 0.5f;
     public GameObject LeftDoor;
     public GameObject RightDoor;
     Transform LeftDoorStartPosition;
     Transform RightDoorStartPosition;
     Vector3 RightDoorMoveDirection;// = Vector3.right;
     Vector3 LeftDoorMoveDirection;// = Vector3.left;
+    public bool IsDoorOpen = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,21 +23,23 @@ public class GateDoorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (LeftDoor.transform.rotation.y >= LeftDoorStartPosition.transform.rotation.y)
+       
+        
+        /*if (LeftDoor.transform.rotation.y != LeftDoorStartPosition.transform.rotation.y)
         {
-            LeftDoorMoveDirection = new Vector3(0, -6, 0);
+            LeftDoorMoveDirection = new Vector3(0, -90, 0);
             Debug.Log("Left");
         }
-        else if (LeftDoor.transform.rotation.y <= - 0.5)
+        else if (LeftDoor.transform.rotation.y <= LeftDoorStartPosition.transform.rotation.y - 90)
         {
-            LeftDoorMoveDirection = Vector3.right;
+            LeftDoorMoveDirection = new Vector3(0, 90, 0);
             Debug.Log("Right");
-        }
+        }*/
 
-        LeftDoor.transform.Rotate(LeftDoorMoveDirection,Space.World);
+        //LeftDoor.transform.Rotate(LeftDoorMoveDirection, Space.World);
         //Debug.Log(LeftDoor.transform.rotation.y);
 
-        if (RightDoor.transform.rotation.y >= RightDoorStartPosition.rotation.x)
+        /*if (RightDoor.transform.rotation.y >= RightDoorStartPosition.rotation.x)
         {
             RightDoorMoveDirection = Vector3.right;
         }
@@ -45,27 +48,27 @@ public class GateDoorScript : MonoBehaviour
             LeftDoorMoveDirection = Vector3.left;
         }
 
-        RightDoor.transform.Translate(RightDoorMoveDirection * Time.deltaTime * liftSpeed);
+        RightDoor.transform.Translate(RightDoorMoveDirection * Time.deltaTime * liftSpeed);*/
 
     }
-    /*
+    
     void OnTriggerEnter(Collider collision)
     {
-        if (transform.position.x >= LeftDoorStartPosition.x)
+        if (!IsDoorOpen)
         {
-            LeftDoorMoveDirection = Vector3.left;
+            LeftDoor.transform.Rotate(new Vector3(0, -90, 0));
+            RightDoor.transform.Rotate(new Vector3(0, 90, 0));
+            IsDoorOpen = true;
         }
-
-        transform.Translate(LeftDoorMoveDirection * Time.deltaTime * liftSpeed);
     }
 
     void OnTriggerExit(Collider collision)
     {
-        if (transform.position.y <= LeftDoorStartPosition.x - 5.5)
+       if (IsDoorOpen)
         {
-            LeftDoorMoveDirection = Vector3.right;
+            LeftDoor.transform.Rotate(new Vector3(0, 0, 0));
+            RightDoor.transform.Rotate(new Vector3(0, 0, 0));
+            IsDoorOpen = false;
         }
-
-        transform.Translate(LeftDoorMoveDirection * Time.deltaTime * liftSpeed);
-    }*/
+    }
 }
