477fc5416b25d37c88465dcdad3f9ce9cf30a6a1
Sydney Brown <sbbrown@email.peace.edu>
2019-10-28 22:54:52 -0400

    Elevator Update

--- Oculus Test 3/Assets/Scripts/Elevator.cs
+++ Oculus Test 3/Assets/Scripts/Elevator.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-	
-	Vector3 moveDirection = Vector3.up;	//elevator starts down
-	float liftSpeed = 5;
-	Vector3 startPos;
     public bool canMove = false;
-	
+
+    private Vector3 moveDirection = Vector3.up; //elevator starts down
+    private float liftSpeed = 5;
+    private Vector3 startPos;
+    private Vector3 stopPos = new Vector3(0, 23, 0);
+    public bool isWait = true;
+    public float waitTime = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +22,48 @@ public class Elevator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Is Elevator Enabled
+        if (canMove == true)
+        {
+            //If Elevator is Waiting at Landing
+            if (isWait == true)
+            {
+                if (waitTime > 0)
+                {
+                    waitTime -= Time.deltaTime;
+                }
+                else if (waitTime <= 0)
+                {
+                    isWait = false;
+                    waitTime = 3.0f;
+                }
 
-        if (canMove == true) {
-            if (transform.position.y >= 30) {
-                moveDirection = Vector3.down;
-            } else if (transform.position.y <= startPos.y) {
-                moveDirection = Vector3.up;
+            }
+            //If Elevator is NOT Waiting at landing
+            else if (isWait == false)
+            {
+                if (transform.position.y > stopPos.y)
+                {
+                    moveDirection = Vector3.down;
+                }
+                else if (transform.position.y < startPos.y)
+                {
+                    moveDirection = Vector3.up;
+                    
+                }
+                else if (transform.position.y == startPos.y)
+                {
+                    isWait = true;
+                }
+                else if (transform.position.y == stopPos.y)
+                {
+                    isWait = true;
+                }
+
+                transform.Translate(moveDirection * Time.deltaTime * liftSpeed);
             }
 
-            transform.Translate(moveDirection * Time.deltaTime * liftSpeed);
         }
-			
+
     }
 }
