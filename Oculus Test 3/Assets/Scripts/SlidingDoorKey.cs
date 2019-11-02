a77f6302bb1a96028948b9ceaa362bb5bc38e4b3
Syd541 <54684449+Syd541@users.noreply.github.com>
2019-11-02 16:34:26 -0400

    sliding door key

---
+++ Oculus Test 3/Assets/Scripts/SlidingDoorKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorKey : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hand")
+        {
+            target.GetComponent<SlidingDoor>().isLocked = false;
+        }
+    }
+}
