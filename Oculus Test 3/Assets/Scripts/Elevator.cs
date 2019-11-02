7c4f39a0a83321d37e83a30cead3374e16b9d705
Syd541 <54684449+Syd541@users.noreply.github.com>
2019-11-02 16:13:38 -0400

    scripts

--- Oculus Test 3/Assets/Scripts/Elevator.cs
+++ Oculus Test 3/Assets/Scripts/Elevator.cs
@@ -8,10 +8,13 @@ public class Elevator : MonoBehaviour
     public GameObject playerRef;                //Reference to the Player GameObject
     public AudioSource elevatorSound;           //"travel" music
     public bool isEnabled = false;              //is elevator working
+    public bool isLocked = false;
     public float elevatorTime = 5.0f;           //time for elevator to "travel"
     public bool playerOn = false;               //is player on elevator
+    public float lockTime = 3.0f;               //Time on landing to lock elevator
     private int currentFloor = 0;               //0= bunker, 1=house
     private Vector3 bunkerPos;                  //Location of the Bunker Platform
+    private Vector3 elevatorHousePos;
     private Vector3 housePos;                   //Location of the House Platform
 
 
@@ -20,8 +23,8 @@ public class Elevator : MonoBehaviour
     {
         //Set Location of both Platforms
         bunkerPos = this.transform.position;
-        housePos = new Vector3(30, 22, 24);
-        //housePos = GameObject.FindGameObjectWithTag("elevatorHouse").transform.position;
+        elevatorHousePos = new Vector3 (0,0,0);
+        housePos = elevatorHousePos + new Vector3(0,1,0);
     }
 
     // Update is called once per frame	
@@ -29,40 +32,46 @@ public class Elevator : MonoBehaviour
     {
         if (isEnabled == true)
         {
-            if (playerOn == true)
+            if (isLocked == false)
             {
-                //If elevator "traveling"
-                if (elevatorTime > 0)
+                if (playerOn == true)
                 {
-                    if (!elevatorSound.isPlaying)
+                    //If elevator "traveling"
+                    if (elevatorTime > 0)
                     {
-                        elevatorSound.Play();
+                        if (!elevatorSound.isPlaying)
+                        {
+                            elevatorSound.Play();
+                        }
+                        elevatorTime -= Time.deltaTime;
                     }
-                    elevatorTime -= Time.deltaTime;
-                }
-                //If elevator done "traveling"
-                else if (elevatorTime <= 0)
-                {
-                    elevatorSound.Stop();
-                    elevatorTime = 5.0f;
-                    playerRef.transform.position = housePos;
+                    //If elevator done "traveling"
+                    else if (elevatorTime <= 0)
+                    {
+                        elevatorSound.Stop();
+                        elevatorTime = 5.0f;
 
-                    //Determine where to move player
-                    if (currentFloor == 0)
-                    {
-                        playerRef.transform.position = housePos;
-                        //currentFloor = 1;
-                    }
-                    else
-                    {
-                        playerRef.transform.position = bunkerPos;
-                        currentFloor = 0;
+                        //Determine where to move player
+                        if (currentFloor == 0)
+                        {
+                            playerRef.transform.position = housePos;
+                            this.transform.position = elevatorHousePos;
+                            currentFloor = 1;
+                            lockElevator();
+                        }
+                        else
+                        {
+                            playerRef.transform.position = bunkerPos;
+                            this.transform.position = bunkerPos;
+                            currentFloor = 0;
+                            lockElevator();
+                        }
                     }
                 }
-            }
-            else
-            {
-                elevatorTime = 5.0f;
+                else
+                {
+                    elevatorTime = 5.0f;
+                }
             }
         }
     }
@@ -82,6 +91,19 @@ public class Elevator : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             playerOn = false;
+        }
+    }
+
+    void lockElevator()
+    {
+        if (lockTime > 0)
+        {
+            isLocked = true;
+            lockTime -= Time.deltaTime;
+        } else if (lockTime <= 0)
+        {
+            isLocked = false;
+            lockTime = 3.0f;
         }
     }
 
