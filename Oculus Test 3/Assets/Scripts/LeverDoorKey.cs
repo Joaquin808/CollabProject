67bdf90c34008e83970f265ba4ad7dcdc87bd265
Platinumsuta <54861300+Platinumsuta@users.noreply.github.com>
2019-11-03 17:29:39 -0500

    dome transparancy & object weights & spatial sound & office lever & bunker window

---
+++ Oculus Test 3/Assets/Scripts/LeverDoorKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverDoorKey : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Lever Cue")
+        {
+            target.GetComponent<SlidingDoor>().isLocked = false;
+        }
+    }
+}
