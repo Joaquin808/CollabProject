1e802d1f3a552f8acbe6fcce9a198b312528947c
Sydney Brown <sbbrown@email.peace.edu>
2019-11-07 09:28:08 -0500

    day/night and dog tweaks

--- Oculus Test 3/Assets/Scripts/DayNightSystem.cs
+++ Oculus Test 3/Assets/Scripts/DayNightSystem.cs
@@ -8,16 +8,16 @@ using UnityEngine;
 public class DayNightSystem : MonoBehaviour
 {
     public float currentTime = 0f;          //current currentTime
-    public float dayLength = 600f;          //DayLength = 10 minutes
+    public float dayLength = 1200f;          //DayLength = 20 minutes
 
     //Skybox References
-    public Material skyOne;					//skybox 1 mins 0-2
-    public Material skyTwo;					//skybox 2 mins 2-4
-    public Material skyThree;				//skybox 3 mins 4-6
-    public Material skyFour;				//skybox 4 mins 6-8
-    public Material skyFive;                //skybox 5 mins 8-9
-    public Material skySix;                 //skybox 6 mins 9-10
-    public Material skySeven;               //skybox 7 mins 10+
+    public Material skyOne;					//skybox 1
+    public Material skyTwo;					//skybox 2
+    public Material skyThree;				//skybox 3
+    public Material skyFour;				//skybox 4
+    public Material skyFive;                //skybox 5
+    public Material skySix;                 //skybox 6
+    public Material skySeven;               //skybox 7
 
     void Update()
     {
@@ -29,16 +29,16 @@ public class DayNightSystem : MonoBehaviour
         //Change Skybox and UpdateEnvironment
         void dayCycle()
         {
-            //Changes at 2-4-6-8-9-10 minutes
+            //Changes at 4-8-12-16-18-20 minutes
             if (currentTime < dayLength * 2 / 10)
-            { //Minutes 0-2
+            { 
                 if (RenderSettings.skybox != skyOne)
                 {
                     RenderSettings.skybox = skyOne;
                     DynamicGI.UpdateEnvironment();
                 }
                 else if (dayLength * 2 / 10 <= currentTime && currentTime <= dayLength * 4 / 10)
-                { //Minutes 2-4
+                { 
                     if (RenderSettings.skybox != skyTwo)
                     {
                         RenderSettings.skybox = skyTwo;
@@ -46,7 +46,7 @@ public class DayNightSystem : MonoBehaviour
                     }
                 }
                 else if (dayLength * 4 / 10 <= currentTime && currentTime <= dayLength * 6 / 10)
-                { //Minutes 4-6
+                { 
                     if (RenderSettings.skybox != skyThree)
                     {
                         RenderSettings.skybox = skyThree;
@@ -54,7 +54,7 @@ public class DayNightSystem : MonoBehaviour
                     }
                 }
                 else if (dayLength * 6 / 10 <= currentTime && currentTime <= dayLength * 8 / 10)
-                { //Minutes 6-8
+                { 
                     if (RenderSettings.skybox != skyFour)
                     {
                         RenderSettings.skybox = skyFour;
@@ -62,7 +62,7 @@ public class DayNightSystem : MonoBehaviour
                     }
                 }
                 else if (dayLength * 8 / 10 <= currentTime && currentTime <= dayLength * 9 / 10)
-                { //Minutes 8-9
+                {
                     if (RenderSettings.skybox != skyFive)
                     {
                         RenderSettings.skybox = skyFive;
@@ -70,7 +70,7 @@ public class DayNightSystem : MonoBehaviour
                     }
                 }
                 else if (dayLength * 9 / 10 <= currentTime && currentTime <= dayLength)
-                {       //Minutes 9-10
+                { 
                     if (RenderSettings.skybox != skySix)
                     {
                         RenderSettings.skybox = skySix;
@@ -78,7 +78,7 @@ public class DayNightSystem : MonoBehaviour
                     }
                 }
                 else if (currentTime >= dayLength)
-                {                                           //Minutes 10+
+                {                                           
                     if (RenderSettings.skybox != skySeven)
                     {
                         RenderSettings.skybox = skySeven;
