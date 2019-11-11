82eb4eeb1d4000c8c047903ebd4dca1c1dd1b499
Platinumsuta <54861300+Platinumsuta@users.noreply.github.com>
2019-11-10 19:47:10 -0500

    Revert "Air Filtration Stuff, please work"
    This reverts commit 15f793eb180d8d06e676b8975246e3bf17bc4543.

--- Oculus Test 3/Assets/Scripts/AirFiltration.cs
+++ Oculus Test 3/Assets/Scripts/AirFiltration.cs
@@ -6,9 +6,8 @@ public class AirFiltration : MonoBehaviour
 {
     public GameObject dial, airFilt;
     Rigidbody rb;
-    float solutionOne, solutionTwo, dialPointed, timer, dialZ;
+    float solutionOne, solutionTwo, dialPointed, timer;
     AudioSource wind;
-    bool inSpot;
     public bool solved;
     //int seconds;
 
@@ -24,111 +23,41 @@ public class AirFiltration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dialPointed = rb.transform.rotation.eulerAngles.x;
-        dialZ = rb.transform.rotation.eulerAngles.z;
-        Debug.Log("Solution One: " + solutionOne);
-        Debug.Log("Solution Two: " + solutionTwo);
-        Debug.Log("Dial: " + dialPointed);
-        Debug.Log("Dialz: " + dialZ);
-        Debug.Log(solved);
+        dialPointed = rb.transform.eulerAngles.x;
         CheckIfSolved();
-        
     }
 
-    //creates a random solution for the air filtration puzzle
     void RandomSolution()
     {
         int x = Random.Range(1, 13);
-        solutionOne = x * 30;
+        solutionOne = (x / 12) * 360;
         solutionTwo = solutionOne + 30;
         if (solutionOne == 360f && solutionTwo == 390f)
         {
-            solutionOne = 30f;
-            solutionTwo = 60f;
+            solutionOne = 0f;
+            solutionTwo = 30f;
         }
-        FixSolutions();
     }
 
     void CheckIfSolved()
     {
-        
-       
-        //solutionOne < dialPointed && dialPointed < solutionTwo is what I used to begin with
-        if (80f < dialZ && dialZ < 100f)
-        {
-            if (solutionTwo < dialPointed && dialPointed < solutionOne)
-            {
-                SolutionFinder();
-            }
-            /*
-            else if (timer < 5)
-            {
-                timer = 0;
-                wind.Stop();
-            }
-            */
-        }
-        if (260f < dialZ && dialZ < 280f)
-        {
-            
-            if (solutionOne < dialPointed && dialPointed < solutionTwo)
-            {
-                SolutionFinder();
-            }
-        }
-
-    }
-
-    //Fixs the solutions to work within unity, hopefully
-    void FixSolutions()
-    {
-        if (solutionOne == 90 && solutionTwo == 120)
-        {
-            solutionOne = 90;
-            solutionTwo = 60;
-        }
-        if (solutionOne == 120)
-        {
-            solutionOne = 60;
-            solutionTwo = 30;
-        }
-        if (solutionOne == 150)
-        {
-            solutionOne = 30;
-            solutionTwo = 0;
-        }
-        if (solutionOne == 180)
-        {
-            solutionOne = 360;
-            solutionTwo = 330;
-        }
-        if (solutionOne == 210)
-        {
-            solutionOne = 330;
-            solutionTwo = 300;
-        }
-        if (solutionOne == 240)
-        {
-            solutionOne = 300;
-            solutionTwo = 270;
-        }
-    }
-
-    void SolutionFinder()
-    {
-        if (timer == 0)
+        if (solutionOne < dialPointed && dialPointed < solutionTwo)
         {
             wind.Play(0);
+            timer = Time.deltaTime;
+            //seconds = Convert.ToInt32(timer % 60);
+            if (timer >= 5)
+            {
+                rb.freezeRotation = true;
+                solved = true;
+            }
         }
-        timer += Time.deltaTime;
-        if (timer >= 5)
+        else if (timer < 5)
         {
-            rb.freezeRotation = true;
-            solved = true;
+            timer = 0;
+            wind.Stop();
         }
-        Debug.Log("Sewconds: " +timer);
     }
-      
 
     void OnBreak()
     {
@@ -139,26 +68,3 @@ public class AirFiltration : MonoBehaviour
         solved = false;
     }
 }
-/*
-           if (dialPointed == .5 || dialPointed == -.5)
-           {
-               if (timer == 0)
-               {
-                   wind.Play(0);
-               }
-               timer += Time.deltaTime;
-               if (timer >= 5)
-               {
-                   rb.freezeRotation = true;
-                   solved = true;
-               }
-               Debug.Log("Wind is working");
-               Debug.Log(timer);
-           }
-           else if (timer < 5)
-           {
-               timer = 0;
-               wind.Stop();
-           }
-       }
-       */
