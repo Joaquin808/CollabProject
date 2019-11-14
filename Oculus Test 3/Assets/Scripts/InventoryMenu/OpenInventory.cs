f5e05ef20a116b94ade59613d5a66daa0bf0d1e2
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-11-14 14:45:52 -0500

    Open inventory change

--- Oculus Test 3/Assets/Scripts/InventoryMenu/OpenInventory.cs
+++ Oculus Test 3/Assets/Scripts/InventoryMenu/OpenInventory.cs
@@ -11,9 +11,35 @@ public class OpenInventory : MonoBehaviour
     public GameObject JournalCanvas;
     public GameObject PlayerCamera;
     RaycastHit Hit;
+    public GameObject LeftHand;
 
-    // Start is called before the first frame update
-    void Start()
+    // use this example code for tooltips for the pickupable items
+    /*void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        public Ray ray;
+        if (Physics.Raycast(ray, out hit, 100))     //Raycast Event
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+
+                var selection = hit.transform;
+        Renderer rend = selection.GetComponent<Renderer>();
+
+
+                if (selection.CompareTag(tag: "Grabable"))      //Outline WIP
+                {
+                    //var selectionRenderer = selection.GetComponent<Renderer>();
+
+                    //if (selectionRenderer != null)
+                    {
+                        //selectionRenderer.material = outline;
+                    }
+
+                    //_selection = selection;
+                }*/
+
+        // Start is called before the first frame update
+        void Start()
     {
         Inventory.SetActive(false);
         IsActive = false;
@@ -23,7 +49,7 @@ public class OpenInventory : MonoBehaviour
     {
         if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward), out Hit, 10000))
         {
-            if (Hit.collider.tag == "InventoryCollider")
+            if (Hit.collider.tag == "InventoryCollider" && LeftHand.transform.rotation.z >= 30 && LeftHand.transform.rotation.z <= 70)
             {
                 Inventory.SetActive(true);
                 IsActive = true;
