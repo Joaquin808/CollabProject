af66ab51784b66de89a39075f261646bf1304ddd
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-11-17 22:14:17 -0500

    Changes
    Made a few changes to the Objectives UI tab to make it look slightly better and have the test fill the spot.

--- Oculus Test 3/Assets/Scripts/InventoryMenu/Objectives.cs
+++ Oculus Test 3/Assets/Scripts/InventoryMenu/Objectives.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Objectives : MonoBehaviour
 {
-    string[] ObjectivesList = {"Find a key to get out of the room", "Find the Power Switch and activate it", "Find the wrench and use it to get access to the Power Station",
+    string[] ObjectivesList = {"Find a key to get out of the room", "Find the Power Switch and activate it", "Find the wrench", "Use the wrench to get access to the Power Station",
         "Complete the puzzle of the Power Station", "Exit the Bunker and find an entrane to the House", "Talk to the AI", "Fix AC Station (House)", "Fix the Water Station (Dome)",
         "Find a way to access the locked room (House)", "Pick up the Temperature Needle", "Fix the Temperature Station (House)", "Find a way to open the main locker (Bunker)",
         "Pick up the key and get access to Grandpa's room", "Use the Radio", "Find a way to open Grandpa's safe", "Listen to Grandpa's message",
