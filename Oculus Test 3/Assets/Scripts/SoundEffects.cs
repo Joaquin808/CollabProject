3229086aace6fdf47f7a1fa926f3b563d9745ef2
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-11-19 15:16:01 -0500

    added general audio source

--- Oculus Test 3/Assets/Scripts/SoundEffects.cs
+++ Oculus Test 3/Assets/Scripts/SoundEffects.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundEffects : MonoBehaviour
 {
+
+    public AudioSource genAudio;
+    public AudioClip[] genSounds;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,14 @@ public class SoundEffects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void findAmbientAudio()
+    {
+        if (genAudio.clip != genSounds[0])
+        {
+
+        }
     }
 }
