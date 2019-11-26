18e6912d6c7192746e0e28d1091ee71295f68375
Syd541 <54684449+Syd541@users.noreply.github.com>
2019-11-26 14:35:43 -0500

    revert 11-25

---
+++ Oculus Test 3/Assets/Scripts/Audio_Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Manager : MonoBehaviour
+{
+    public AudioClip Erokia;
+    public AudioClip acPuzzle; //Music Track for AC Puzzle
+    public AudioClip aiCrazy;
+    public AudioClip ambientChoirChanting;
+    public AudioClip ambientWind;
+    public AudioClip angelsBehindTheWall;
+    public AudioClip bsideAmbient;
+    public AudioClip blueZoneAmbience;
+    public AudioClip blueZoneRise;
+    public AudioClip blueZoneDarkAmbience; //Starting Room Ambient Sound
+    public AudioClip Desolation;
+    public AudioClip droneDungeonRumble;
+    public AudioClip Earthquake;
+    public AudioClip firstTimeOutside; //Music 
+    public AudioClip gameOver; //Game Over Music Track
+    public AudioClip hollowDrone;
+    public AudioClip lowDrone;
+    public AudioClip manifestDrone;
+    public AudioClip pipePuzzle; //Music Track for Pipe Puzzle
+    public AudioClip powerPuzzle; //Music Track for Power Puzzle
+    public AudioClip radioPuzzle; //Music Track for Radio Puzzle
+    public AudioClip temperaturePuzzle; //Music Track for Temperature Puzzle
+    public AudioClip Whirlwind;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
