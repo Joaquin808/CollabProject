16206901be409e7f2f99a4ab79329dd0166bfdb4
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-12-01 18:44:42 -0500

    Changes

--- Oculus Test 3/Assets/Scripts/SlidingDoor.cs
+++ Oculus Test 3/Assets/Scripts/SlidingDoor.cs
@@ -11,14 +11,15 @@ public class SlidingDoor : MonoBehaviour
     public bool isLocked = false;           //Is Key Owned by Player
     Vector3 startPos;
     Vector3 endPos;
-    public SoundEffects soundFX;
+    SoundEffects soundFX;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = this.transform.position;
         endPos = startPos - new Vector3(0, 5, 0);
-        
+
+        soundFX = GameObject.FindGameObjectWithTag("Player").GetComponent<SoundEffects>();    
     }
 
     // Update is called once per frame
