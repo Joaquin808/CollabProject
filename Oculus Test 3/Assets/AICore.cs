b5ecdd7e680068e6f2c01331c323671712e2e989
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-12-03 15:14:08 -0500

    All AI messages done. Increased Journal Entry text size

--- Oculus Test 3/Assets/AICore.cs
+++ Oculus Test 3/Assets/AICore.cs
@@ -6,11 +6,13 @@ public class AICore : MonoBehaviour
 {
     bool TalkedToAIFirstTime = false;
     SoundEffects SFXScript;
+    Objectives ObjScript;
 
     // Start is called before the first frame update
     void Start()
     {
         SFXScript = GameObject.Find("OVRPlayerController").GetComponent<SoundEffects>();
+        ObjScript = GameObject.Find("OVRPlayerController").GetComponent<Objectives>();
     }
 
     // Update is called once per frame
@@ -23,9 +25,33 @@ public class AICore : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            if (TalkedToAIFirstTime == false)
+
+            if (ObjScript.ObjectiveNumber == 2)
             {
-                //SFXScript.genAudio.Play(SFXScript.genSounds[22]);
+                SFXScript.genAudio.Stop();
+                SFXScript.genAudio.clip = SFXScript.genSounds[22];
+                SFXScript.genAudio.Play();
+            }
+
+            if (ObjScript.ObjectiveNumber == 3)
+            {
+                SFXScript.genAudio.Stop();
+                SFXScript.genAudio.clip = SFXScript.genSounds[28];
+                SFXScript.genAudio.Play();
+            }
+
+            if (ObjScript.ObjectiveNumber == 4)
+            {
+                SFXScript.genAudio.Stop();
+                SFXScript.genAudio.clip = SFXScript.genSounds[26];
+                SFXScript.genAudio.Play();
+            }
+
+            if (ObjScript.ObjectiveNumber == 5)
+            {
+                SFXScript.genAudio.Stop();
+                SFXScript.genAudio.clip = SFXScript.genSounds[27];
+                SFXScript.genAudio.Play();
             }
         }
     }
