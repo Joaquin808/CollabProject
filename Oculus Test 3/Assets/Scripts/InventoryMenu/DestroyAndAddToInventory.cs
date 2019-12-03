274481ae86415ab4e9f5da196586866300d397d3
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-12-03 15:53:07 -0500

    Changes

--- Oculus Test 3/Assets/Scripts/InventoryMenu/DestroyAndAddToInventory.cs
+++ Oculus Test 3/Assets/Scripts/InventoryMenu/DestroyAndAddToInventory.cs
@@ -1,11 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DestroyAndAddToInventory : MonoBehaviour
 {
     public Pickup InventoryItemRef;
     //public bool CanAddToInventory;
+    bool ItemWasAdded = false;
+    public Text ItemAddedText;
+    float Timer;
+
+    void Start()
+    {
+        ItemAddedText = GameObject.Find("ItemAddedTextParent").GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        if (ItemWasAdded)
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= 5)
+            {
+                ItemAddedText.text = InventoryItemRef.name + " was added to the inventory";
+                ItemWasAdded = false;
+            }
+            else
+            {
+                ItemAddedText.text = "";
+            }
+        }
+   
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +43,7 @@ public class DestroyAndAddToInventory : MonoBehaviour
             rend.enabled = false;
             Rigidbody rigid = gameObject.GetComponent<Rigidbody>();
             rigid.detectCollisions = false;
+            ItemWasAdded = true;
         }
     }
 }
