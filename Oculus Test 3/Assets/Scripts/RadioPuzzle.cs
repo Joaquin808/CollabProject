6de1f31df05c6cf1543893b9d725472f9db5a155
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-12-09 11:20:52 -0500

    changes

--- Oculus Test 3/Assets/Scripts/RadioPuzzle.cs
+++ Oculus Test 3/Assets/Scripts/RadioPuzzle.cs
@@ -7,6 +7,8 @@ public class RadioPuzzle : MonoBehaviour
     Objectives objectives;
     AudioSource message;
     Rigidbody rb;
+    public bool radioOn = true;
+    public float timer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,27 @@ public class RadioPuzzle : MonoBehaviour
         message = GetComponent<AudioSource>();
     }
 
+    void Update()
+    {
+        if (radioOn)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 68)
+        {
+            radioOn = false;
+            objectives.SetNextObjective();
+            timer = 0;
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Sphere" && objectives.ObjectiveNumber == 5)
         {
             rb.freezeRotation = true;
             message.Play(0);
-            objectives.SetNextObjective();            
+            radioOn = true;
         }
       
     }
