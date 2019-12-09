028823346fb1221c8a8c33270baf446a51dfcea9
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2019-12-09 10:43:03 -0500

    changes

--- Oculus Test 3/Assets/Scripts/ThatCollider.cs
+++ Oculus Test 3/Assets/Scripts/ThatCollider.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class ThatCollider : MonoBehaviour
 {
     public bool itIn;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     
 
     void OnTriggerEnter(Collider other)
